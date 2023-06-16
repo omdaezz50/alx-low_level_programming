c6d89a37da3915ef2cc91aecfb78063297bfb74d
“Emad <omdaezz50@gmail.com>
2023-06-16 13:55:12 -0700

    done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,8 @@ int main(void)
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
-		printf("%i is zero\n", n);
-	else
 		printf("%i is negative\n", n);
+	else
+		printf("%i is Zero\n", n);
 	return (0);
 }
