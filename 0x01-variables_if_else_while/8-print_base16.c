9f814fc37d9ccb1557be04d935ada3b446d8ffe1
“Emad <omdaezz50@gmail.com>
2023-06-19 15:26:34 -0700

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print the alphabet in reverse order in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else
+			putchar(i - 10 + 'a');
+	}
+	putchar('\n');
+	return (0);
+}
